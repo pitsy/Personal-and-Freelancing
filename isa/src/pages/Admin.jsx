ce1015088885a30c055c6000ca64af71dcb03ef5
pitsy <rainer.pits@gmail.com>
2022-10-31 20:49:02 +0200

    much improved keyword filtering added CategoryGallery and Admin
    I love useEffect now

--- isa/src/pages/Admin.jsx
+++ isa/src/pages/Admin.jsx
@@ -14,6 +14,11 @@ function Admin() {
     const [showAlert, setShowAlert] = useState(false);
     const [tempPicId, setTempPicId] = useState('');
 
+    // keyword variables
+    let receivedKeywords = [];
+    const [usedKeywords, setUsedKeywords] = useState([]);
+    const [selectedKeywords, setSelectedKeywords] = useState([]);
+
     useEffect(() => {
         fetch('https://isaleht-7a2e8-default-rtdb.europe-west1.firebasedatabase.app/pictures.json')
             .then(res => res.json())
@@ -22,12 +27,19 @@ function Admin() {
             });
     }, []);
 
+    useEffect(() => {
+        receivedKeywords = [];
+        filteredPictures.forEach(element => receivedKeywords.push(...element.keywords));
+        setUsedKeywords([...new Set(receivedKeywords)].sort());
+    }, [filteredPictures]);
+
     function filterByCategory(categoryClicked) {
+        setSelectedKeywords([]);
         if (categoryClicked === 'all') {
             setFilteredPictures(pictures);
             setActiveCategory('all');
         } else {
-            const result = pictures.filter(element => element.category === categoryClicked);
+            let result = pictures.filter(element => element.category === categoryClicked);
             setFilteredPictures(result);
             setActiveCategory(categoryClicked);
         }
@@ -52,12 +64,46 @@ function Admin() {
     }
 
     function saveKeywords(keywords) {
-        const selectedKeywords = JSON.stringify(keywords);
-        sessionStorage.setItem('keywords', selectedKeywords)
+        const getKeywords = JSON.stringify(keywords);
+        sessionStorage.setItem('keywords', getKeywords)
     }
-    
+
+    // handle filtering pictures by keywords
+    function searchKeyword(wordClicked) {
+        // check if keyword is already selected
+        const index = selectedKeywords.findIndex(element => element === wordClicked);
+        if (index >= 0) {
+            // remove keyword if it is already selected
+            selectedKeywords.splice(index,1);
+        } else {
+            // add the new keyword if it was not previously selected
+            selectedKeywords.push(wordClicked);
+        }
+        setSelectedKeywords(selectedKeywords.slice());
+        // filter pictures which include the selected keywords
+        let filteredResult = [];
+        // separate cases since 'all' includes all categories
+        if (activeCategory === 'all') {
+            filteredResult = pictures;
+            for (let i = 0; i < selectedKeywords.length; i++) {
+                filteredResult = filteredResult.filter(element => element.keywords.indexOf(selectedKeywords[i]) >= 0);
+                setFilteredPictures(filteredResult);
+            }
+        } else {
+            filteredResult = pictures.filter(element => element.category === activeCategory);
+            for (let i = 0; i < selectedKeywords.length; i++) {
+                filteredResult = filteredResult.filter(element => element.keywords.indexOf(selectedKeywords[i]) >= 0);
+                setFilteredPictures(filteredResult);
+            }
+        }
+        if (selectedKeywords.length === 0) {
+            filterByCategory(activeCategory);
+        }
+    }
+
     return ( 
         <div className='page'>
+            {/* alert to confirm/cancel picture delete */}
             <Alert show={showAlert}>
                 <Alert.Heading>Tahad sa kindlasti selle pildi kustutada?</Alert.Heading>
                 <div>
@@ -65,6 +111,7 @@ function Admin() {
                     <Button variant='outline-primary' className={styles.alertBtn} onClick={() => setShowAlert(false)}>Ei</Button>
                 </div>                
             </Alert>
+            {/* map buttons for categories */}
             <span className={styles.categoryBtn}><b>Categories:</b></span>
             <Button variant='outline-primary' size='sm' className={styles.categoryBtn} onClick={() => filterByCategory('all')}>all</Button>
             {categories.map(category => 
@@ -72,13 +119,29 @@ function Admin() {
                     <Button variant='outline-primary' size='sm' className={styles.categoryBtn} onClick={() => filterByCategory(category)}>
                         {category}
                     </Button>
-                </span> )}            
+                </span> )}
+            {/* various buttons */}
             <div>
                 <Link to='/add-picture'>
                     <Button variant='outline-primary' size='lg' className={styles.categoryBtn}>Add picture</Button>
                 </Link>
                 <Button variant='outline-primary' size='lg' className={styles.logoutBtn} onClick={() => authCtx.updateLoggedIn(false)}>Log out</Button>
             </div>
+            {/* keyword selection */}
+            <div>
+                {usedKeywords.map(element => 
+                    <Button className={styles.mapKeywordBtn} key={element} size='sm' variant='outline-light'
+                    onClick={() => searchKeyword(element)}>
+                        {element}
+                    </Button> )}
+                {selectedKeywords.map(element => 
+                <Button
+                    key={element} className={styles.mapKeywordBtn} size='sm' variant='light'
+                    onClick={() => searchKeyword(element)}>
+                        {element}
+                </Button>)}
+            </div>
+            {/* display the picture cards */}
             <div className={styles.gridContainer}>
             {filteredPictures.map(element =>
                 <div key={element.id}>
--- isa/src/pages/CategoryGallery.jsx
+++ isa/src/pages/CategoryGallery.jsx
@@ -8,49 +8,42 @@ function CategoryGallery() {
     const [dbPictures, setDbPictures] = useState([]);
     const [pictureIndex, setPictureIndex] = useState(0);
     const [imgActive, setImgActive] = useState(false);
-    const [selectedKeywords, setSelectedKeywords] = useState([]);
+    // const [selectedKeywords, setSelectedKeywords] = useState([]);
 
     const {category} = useParams(); // useParams always comes as a string
 
-    // create array of unique keywords used in the pictures displayed
-    // dbKeywords alternative is necessary to prevent error on category switch
-    let usedKeywords = [];
-    let usedDbKeywords = [];
-    pictures.filter(element => element.category === category).forEach(element => addArrays(element.keywords));
-    const uniqueUsedKeywords = [...new Set(usedKeywords)];
-    dbPictures.filter(element => element.category === category).forEach(element => addDbArrays(element.keywords));
-    const uniqueUsedDbKeywords = [...new Set(usedDbKeywords)];
+    // keyword variables
+    let receivedKeywords = [];
+    const [usedKeywords, setUsedKeywords] = useState([]);
+    const [selectedKeywords, setSelectedKeywords] = useState([]);
     
     useEffect(() => {
         fetch('https://isaleht-7a2e8-default-rtdb.europe-west1.firebasedatabase.app/pictures.json')
             .then(res => res.json())
             .then(data => {
-                setPictures(data || []);
+                setPictures(data.filter(element => element.category === category) || []);
                 setDbPictures(data || []);
             });
     }, []);
 
-    function addArrays(array) {
-        usedKeywords.push(...array);
-    }
+    useEffect(() => {
+        setPictures(dbPictures.filter(element => element.category === category));
+        setSelectedKeywords([]);
+    }, [category, dbPictures]);
 
-    function addDbArrays(array) {
-        usedDbKeywords.push(...array);
-    }
+    useEffect(() => {
+        receivedKeywords = [];
+        pictures.forEach(element => receivedKeywords.push(...element.keywords));
+        setUsedKeywords([...new Set(receivedKeywords)].sort());
+    }, [pictures]);
 
     function setActivePicture(picClicked) {
-        const check = pictures.filter(element => element.category === category).length;
-        if (check === 0) {
-            setPictures(dbPictures.slice());
-            selectedKeywords.splice(0,selectedKeywords.length)
-            setSelectedKeywords(selectedKeywords.slice());
-        }
         setImgActive(true);
         setPictureIndex(picClicked);
     }
 
     function nextImage() {
-        if (pictureIndex <= (pictures.filter(element => element.category === category).length - 2)) {
+        if (pictureIndex <= (pictures.length - 2)) {
             let index = pictureIndex;
             index += 1;
             setPictureIndex(index);
@@ -71,13 +64,6 @@ function CategoryGallery() {
 
     // handle filtering pictures by keywords
     function searchKeyword(wordClicked) {
-        // initial check if pictures is empty -> empty selectedKeywords 
-        // the keywords from the previous category carry over
-        const check = pictures.filter(element => element.category === category).length;
-        if (check === 0) {
-            selectedKeywords.splice(0,selectedKeywords.length)
-            setSelectedKeywords(selectedKeywords.slice());
-        }
         // check if keyword is already selected
         const index = selectedKeywords.findIndex(element => element === wordClicked);
         let filteredResult = dbPictures.filter(element => element.category === category);
@@ -103,25 +89,15 @@ function CategoryGallery() {
     return ( 
         <div className='page'>
             <div className={styles.searchContainer}>
-                {uniqueUsedKeywords.map(element =>
+                {usedKeywords.map(element =>
                 // check if keyword has been selected before
                     <Button key={element} className={styles.keywordBtn} size='sm' variant='outline-light'
                         onClick={() => searchKeyword(element)}>
                             {element}
                     </Button>
                 )}
-                {uniqueUsedKeywords.length === 0 && 
-                // run incase unique keywords array becomes empty upon category change
-                // doing so maintains the function of filtering the displayed keyword options
-                    uniqueUsedDbKeywords.map(element => 
-                        <Button key={element} className={styles.keywordBtn} size='sm' variant='outline-light'
-                            onClick={() => searchKeyword(element)}>
-                                {element}
-                        </Button>
-                        )
-                }
-                {/* hide this if the category was switched while keywords were selected */}
-                {pictures.filter(element => element.category === category).length !== 0 && selectedKeywords.map(element => 
+                {/* display selected keywords */}
+                {selectedKeywords.map(element => 
                     <Button
                         key={element}
                         className={styles.keywordBtn}
@@ -134,26 +110,17 @@ function CategoryGallery() {
             </div>
             { imgActive && <div className={styles.popup}>
                 <div className={styles.topBar}>
-                    <p>{pictures.filter(element => element.category === category)[pictureIndex].name}</p>
+                    <p>{pictures[pictureIndex].name}</p>
                     <img onClick={() => setImgActive(false)} className={styles.closeBtn} src={require('../images/close.png')} alt="" />
                 </div>
                 <img onClick={nextImage} className={styles.rightBtn} src={require('../images/arrow.png')} alt="" />
                 <img onClick={previousImage} className={styles.leftBtn} src={require('../images/arrow.png')} alt="" />
-                <img className={styles.largeImage} src={pictures.filter(element => element.category === category)[pictureIndex].bigpicture} alt="" />
+                <img className={styles.largeImage} src={pictures[pictureIndex].bigpicture} alt="" />
             </div> }
             <br />
             {/* picture gallery */}
             <div className={styles.gridContainer}>
-                {pictures.filter(element => element.category === category).length === 0 && 
-                // case if pictures are filtered by keyword and then category changes (the pictures array becomes empty)
-                // the fix in this case is to use dbPictures; this resets on reload or keyword filter
-                    dbPictures.filter(element => element.category === category).map((element, index) =>
-                    <div key={element.id}>
-                        <Card onClick={() => setActivePicture(index)} className={styles.image} style={{ width: '18rem' }} bg='light'>
-                            <Card.Img src={element.thumbnail} alt={element.name} />
-                        </Card>
-                    </div> )}
-                {pictures.filter(element => element.category === category).map((element, index) =>
+                {pictures.map((element, index) =>
                     <div key={element.id}>
                         <Card onClick={() => setActivePicture(index)} className={styles.image} style={{ width: '18rem' }} bg='light'>
                             <Card.Img src={element.thumbnail} alt={element.name} />
